37bcb4b66f12523a55c9bd92644d0cabf42d7b48
hoho00 <jhoya963@gmail.com>
2023-09-21 11:25:02 +0900

    init: first commit

---
+++ src/main/java/com/hoho00/game/EatingpizzaApplication.java
@@ -0,0 +1,13 @@
+package com.hoho00.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatingpizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatingpizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoho00/game/EatingpizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoho00.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatingpizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
