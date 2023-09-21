792d011b66999d373517f67452cdd8db7678706c
hoho00 <jhoya963@gmail.com>
2023-09-21 17:44:01 +0900

    Feat: make a Member Entity

---
+++ src/main/java/com/hoho00/game/domain/entity/Member.java
@@ -0,0 +1,33 @@
+package com.hoho00.game.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_member")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", unique = true, nullable = false)
+    private Long memberId;
+
+    @Column(columnDefinition = "VARCHAR(20)")
+    private String name;
+
+    @Column(columnDefinition = "VARCHAR(225)")
+    private String image;
+
+    @Column(columnDefinition = "INT")
+    private Integer level;
+
+    @Column(columnDefinition = "INT")
+    private Integer exp;
+}
